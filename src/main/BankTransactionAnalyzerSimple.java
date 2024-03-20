ae27236077b37206b9680a4d9afe26577ddb3b2a
ahmed.tantawy <ahmed.tantawy@sewedy.com.eg>
2024-03-20 22:54:10 +0200

    Initial commit

---
+++ src/main/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+
+public class BankTransactionAnalyzerSimple {
+
+    private static final String RESOURCES = "src/main/resources/";
+    public static void main(final String... args) throws IOException {
+        final Path path = Paths.get(RESOURCES + args[0]);
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        for (final String line: lines){
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+
+        System.out.println("The total for all transactions is " + total);
+
+
+
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        double januaryTotal = 0d;
+        for (final String line: lines){
+            final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if (date.getMonth() == Month.JANUARY){
+
+            }
+        }
+
+        System.out.println("The total for all transactions is " + total);
+    }
+}
